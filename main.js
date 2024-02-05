41b64420b2ef2575a861855d731e12da97da7868
ItsKyyy <152868326+ItsKyyy@users.noreply.github.com>
2024-02-05 11:54:13 +0700

    Update main.js
    added main.js

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ const noBtn = document.querySelector(".no-btn");
 yesBtn.addEventListener("click", () => {
     question.innerHTML = "Lol, Caught In 4K bro";
     gif.src =
-    "https://www.google.com/url?sa=i&url=https%3A%2F%2Ftenor.com%2Fview%2Fcaught-you-in-4k-meme-gif-7952073828116424104&psig=AOvVaw28kiN0OTpzIiccsRZLSLVZ&ust=1707194771004000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCOi-y6Gyk4QDFQAAAAAdAAAAABAE"; 
+    "https://media1.tenor.com/m/bltxA8iPZagAAAAC/caught-you-in-4k-meme.gif"; 
 });
 
 noBtn.addEventListener("mouseover", () => {
